397fb513e4d04f0b96085e02238e8bc9ce81635b
EJ <ejlayco@gmail.com>
2023-07-15 01:20:56 +0800

    added presenter notes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 console.info(`Loading presentation: ${process.argv[2]}`);
 app.use(express.static(process.argv[2]));
+app.use('/reveal/plugin', express.static(__dirname + '/node_modules/reveal.js/plugin'));
+app.use('/reveal', express.static(__dirname + '/node_modules/reveal.js/dist'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.listen(port, () => {
   console.log(`Portfolio running at port ${port}.`);
