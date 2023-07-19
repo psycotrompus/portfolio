8f43ad43d07e9013986a3eec1388fecae3b2f553
EJ <ejlayco@gmail.com>
2023-07-19 19:46:59 +0800

    added technical skills section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 console.info(`Loading presentation: ${process.argv[2]}`);
 app.use(express.static(process.argv[2]));
